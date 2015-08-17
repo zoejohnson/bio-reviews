28339a10398c90b4671ccf228b0c784cc5363907
zoejohnson <zmn.johnson@gmail.com>
2015-08-16 18:35:36 -0700

    First review

---
+++ queue_database.js
@@ -0,0 +1,19 @@
+ethnicitySpecificOralMicrobiome =	{
+	  "title": "Deep Sequencing Identifies Ethnicity-Specific Bacterial Signatures in the Oral Microbiome",
+	  "url" :"http://journals.plos.org/ploscollections/article?id=10.1371/journal.pone.0077287"
+	}
+
+vaginalMicrobiome = {
+	"title": "Vaginal microbiome of reproductive-age women",
+	"url": "http://www.pnas.org/content/108/Supplement_1/4680"
+	}
+
+enterotypeCommunityStructures = {
+	"title": "A Guide to Enterotypes across the Human Body: Meta-Analysis of Microbial Community Structures in Human Microbiome Datasets",
+	"url": "http://journals.plos.org/ploscollections/article?id=10.1371/journal.pcbi.1002863"
+	}
+
+
+queue_microbiome = [ethnicitySpecificOralMicrobiome, vaginalMicrobiome, enterotypeCommunityStructures]
+
+module.exports = queue_microbiome;
